152bcdbd46d6328cf675a72c10fd15d955063a2b
rishikesh1504 <rishikesh.kyamaji@eaglegenomics.com>
2022-02-10 12:33:37 +0530

    test file updated

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -4,12 +4,6 @@
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
-
-describe command('python --version') do
-  its(:exit_status) { should eq 0 }
-  its(:stdout) { should match('Python 3.7.2') }
-end
-
 describe command('pyenv version') do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should include('3.7.2') }
